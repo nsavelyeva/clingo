cbb8e2b83158c2e6035aeb188438b1f5441c9363
Natallia Savelyeva <n.savelyeva@thebeat.co>
2022-02-23 14:46:16 +0100

    Addressed PR checks

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -13,32 +14,47 @@ import (
 
 func TestPrecedence(t *testing.T) {
 	// Run the tests in a temporary directory
-	tmpDir, err := ioutil.TempDir("..", "tmp-test-clingo")
-	require.NoError(t, err, "error creating a temporary test directory")
-	defer os.Remove(tmpDir)
-	testDir, err := os.Getwd()
-	require.NoError(t, err, "error getting the current working directory")
-	defer os.Chdir(testDir)
-	err = os.Chdir(tmpDir)
-	require.NoError(t, err, "error changing to the temporary test directory")
+	tmpDir, e1 := ioutil.TempDir("..", "tmp-test-clingo")
+	require.NoError(t, e1, fmt.Sprintf("error creating a temporary test folder %s", tmpDir))
+
+	defer func(name string) {
+		e2 := os.Remove(name)
+		require.NoError(t, e2, fmt.Sprintf("error removing temporary test folder %s", name))
+	}(tmpDir)
+
+	testDir, e3 := os.Getwd()
+	require.NoError(t, e3, "error getting the current working directory")
+
+	defer func(dir string) {
+		e4 := os.Chdir(dir)
+		require.NoError(t, e4, fmt.Sprintf("error changing working directory to %s", dir))
+	}(testDir)
+
+	e5 := os.Chdir(tmpDir)
+	require.NoError(t, e5, fmt.Sprintf("error changing to the temporary test directory %s", tmpDir))
 
 	// Set favorite-color with the config file
 	t.Run("config file", func(t *testing.T) {
 		// Copy the config file into our temporary test directory
-		configB, e1 := ioutil.ReadFile(filepath.Join(testDir, "..", "clingo-conf.toml"))
-		require.NoError(t, e1, "error reading test config file")
-		e2 := ioutil.WriteFile(filepath.Join(tmpDir, "clingo-conf.toml"), configB, 0644)
-		require.NoError(t, e2, "error writing test config file")
-		defer os.Remove(filepath.Join(tmpDir, "clingo-conf.toml"))
+		readPath := filepath.Join(testDir, "..", "clingo-conf.toml")
+		configB, e6 := ioutil.ReadFile(readPath)
+		require.NoError(t, e6, fmt.Sprintf("error reading test config file %s", readPath))
+
+		writePath := filepath.Join(tmpDir, "clingo-conf.toml")
+		e7 := ioutil.WriteFile(writePath, configB, 0644)
+		require.NoError(t, e7, fmt.Sprintf("error writing test config file %s", writePath))
+
+		defer func(name string) {
+			e8 := os.Remove(name)
+			require.NoError(t, e8, fmt.Sprintf("error removing test config file %s", name))
+		}(filepath.Join(tmpDir, "clingo-conf.toml"))
 
 		// Run ./clingo
 		cmd := NewRootCommand()
 		output := &bytes.Buffer{}
 		cmd.SetOut(output)
-		err = cmd.Execute()
-		if err != nil {
-			return
-		}
+		err := cmd.Execute()
+		require.NoError(t, err, "error executing cli command")
 
 		gotOutput := output.String()
 		wantOutput := `Your favorite color is: blue
@@ -50,18 +66,19 @@ The magic number is: 7
 	// Set favorite-color with an environment variable
 	t.Run("env var", func(t *testing.T) {
 		// Run CLINGO=purple ./clingo
-		_ = os.Setenv("CLINGO_FAVORITE_COLOR", "purple")
+		e6 := os.Setenv("CLINGO_FAVORITE_COLOR", "purple")
+		require.NoError(t, e6, "error setting CLINGO_FAVORITE_COLOR env variable")
+
 		defer func() {
-			_ = os.Unsetenv("CLINGO_FAVORITE_COLOR")
+			e7 := os.Unsetenv("CLINGO_FAVORITE_COLOR")
+			require.NoError(t, e7, "error unsetting CLINGO_FAVORITE_COLOR env variable")
 		}()
 
 		cmd := NewRootCommand()
 		output := &bytes.Buffer{}
 		cmd.SetOut(output)
 		err := cmd.Execute()
-		if err != nil {
-			return
-		}
+		require.NoError(t, err, "error executing cli command")
 
 		gotOutput := output.String()
 		wantOutput := `Your favorite color is: purple
@@ -78,9 +95,7 @@ The magic number is: 7
 		cmd.SetOut(output)
 		cmd.SetArgs([]string{"--number", "2"})
 		err := cmd.Execute()
-		if err != nil {
-			return
-		}
+		require.NoError(t, err, "error executing cli command")
 
 		gotOutput := output.String()
 		wantOutput := `Your favorite color is: red
